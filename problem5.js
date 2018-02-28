044822d7cf632d5939a8e827a50d0efcea171944
Kaitlyn Menghini <kaitlyn.menghini@gmail.com>
2018-02-28 01:34:39 -0800

    complete problem5

--- problem5.js
+++ problem5.js
@@ -5,3 +5,21 @@
 
 // What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
+function smallestMultiple() {
+  var acc = false;
+  var n = 2520;
+  while (!acc) {
+    isDivisibleToY(n, 20) ? acc = true : n++
+  }
+  return n;
+}
+
+function isDivisibleToY(x, y) {
+  var acc = true;
+  for (var i = 1; i <= y; i++) {
+    acc = acc && (x % i === 0);
+  }
+  return acc;
+}
+
+console.log(smallestMultiple())
